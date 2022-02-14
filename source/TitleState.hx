faa7046fcfb6fda93a8010989bbd70f12a0e3ff1
Spongey <71215126+Spongeyboi@users.noreply.github.com>
2022-02-14 09:49:26 -0500

    Update TitleState.hx

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -90,7 +90,8 @@ class TitleState extends MusicBeatState
 		});
 		#end
 	}
-
+	
+	var background:FlxSprite;
 	var logoBl:FlxSprite;
 	var gfDance:FlxSprite;
 	var danceLeft:Bool = false;
@@ -142,12 +143,16 @@ class TitleState extends MusicBeatState
 		logoBl.updateHitbox();
 		// logoBl.screenCenter();
 		// logoBl.color = FlxColor.BLACK;
-
+		
+		background = new FlxSprite(FlxG.width, FlxG.height);
+		background.frames = Paths.getSparrowAtlas('titleBg');
+		
 		gfDance = new FlxSprite(FlxG.width * 0.4, FlxG.height * 0.07);
 		gfDance.frames = Paths.getSparrowAtlas('gfDanceTitle');
 		gfDance.animation.addByIndices('danceLeft', 'gfDance', [30, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14], "", 24, false);
 		gfDance.animation.addByIndices('danceRight', 'gfDance', [15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29], "", 24, false);
 		gfDance.antialiasing = true;
+		add(background);
 		add(gfDance);
 		add(logoBl);
 
